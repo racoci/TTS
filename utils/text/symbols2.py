98057661dd214a804dcae952aa26cea2550ab337
Edresson Casanova <edresson1@gmail.com>
2020-02-29 15:58:49 -0300

    text parameters added in config.json

--- utils/text/symbols2.py
+++ utils/text/symbols2.py
@@ -5,18 +5,6 @@ Defines the set of symbols used in text input to the model.
 The default is a set of ASCII characters that works well for English or text that has been run
 through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details.
 '''
-def make_symbols(characters, phonemes, punctuations='!\'(),-.:;? ', pad='_', eos='~', bos='^'):
-    ''' Function to create symbols and phonemes '''
-    _phonemes = sorted(list(phonemes))
-
-    # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
-    _arpabet = ['@' + s for s in _phonemes]
-
-    # Export all symbols:
-    symbols = [pad, eos, bos] + list(characters) + _arpabet
-    phonemes = [pad, eos, bos] + list(_phonemes) + list(punctuations)
-
-    return symbols, phonemes
 
 _pad = '_'
 _eos = '~'
@@ -32,9 +20,14 @@ _pulmonic_consonants = 'pbtdʈɖcɟkɡqɢʔɴŋɲɳnɱmʙrʀⱱɾɽɸβfvθðsz�
 _suprasegmentals = 'ˈˌːˑ'
 _other_symbols = 'ʍwɥʜʢʡɕʑɺɧ'
 _diacrilics = 'ɚ˞ɫ'
-_phonemes = _vowels + _non_pulmonic_consonants + _pulmonic_consonants + _suprasegmentals + _other_symbols + _diacrilics
+_phonemes = sorted(list(_vowels + _non_pulmonic_consonants + _pulmonic_consonants + _suprasegmentals + _other_symbols + _diacrilics))
 
-symbols, phonemes = make_symbols( _characters, _phonemes,_punctuations, _pad, _eos, _bos)
+# Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
+_arpabet = ['@' + s for s in _phonemes]
+
+# Export all symbols:
+symbols = [_pad, _eos, _bos] + list(_characters) + _arpabet
+phonemes = [_pad, _eos, _bos] + list(_phonemes) + list(_punctuations)
 
 # Generate ALIEN language
 # from random import shuffle
@@ -45,3 +38,4 @@ if __name__ == '__main__':
     print(symbols)
     print(" > TTS phonemes {}".format(len(phonemes)))
     print(phonemes)
+
